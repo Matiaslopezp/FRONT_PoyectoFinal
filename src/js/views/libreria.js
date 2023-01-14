b61ef853cd952940b4df8297dd7a07c8380dc05e
Naturalis007 <tamara.salasvergara@gmail.com>
2023-01-14 03:55:43 +0000

    creación componente carousel para view librería

--- src/js/views/libreria.js
+++ src/js/views/libreria.js
@@ -3,6 +3,11 @@ import React from "react";
 
 
 export const Libreria = ()=> {
-    return <h1>Librería digital</h1>
-           <h2>Aquí encontrarás diversos recursos audiovisuales que te permitirán seguir aprendiendo la Lengua de Señas Chilena de una manera entretenida</h2>
+    return 
+    <div className="container ">
+        <h1>Librería digital</h1>
+    </div>
 }
+    
+    
+    
