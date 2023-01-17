3f6e74e42f650fc83ded884c3a02e26b72780579
Naturalis007 <tamara.salasvergara@gmail.com>
2023-01-17 14:14:06 +0000

    inserté img a las cards

--- src/js/component/card-libreria.js
+++ src/js/component/card-libreria.js
@@ -8,7 +8,7 @@ export const CardHorizontal = (props) => {
             <div className="card mb-3" style={{"max-width": "540px"}} >
                 <div className="row g-0">
                     <div className="col-md-4">
-                        <img src="..." className="img-fluid rounded-start" alt="..." />
+                        <img src={props.img} className="img-fluid rounded-start" alt="imagen de referencia" />
                     </div>
                     <div className="col-md-8">
                         <div className="card-body">
--- src/js/views/libreria.js
+++ src/js/views/libreria.js
@@ -12,22 +12,26 @@ export const Libreria = ()=> {
         </h2>
         <div className="row-6">
             <CardHorizontal titulo="Mazapán en Lengua de Señas"
+                            img="https://files.wapa.pe/Wapa/2019/02/17/se-as8-1550427868.jpg"
                             descripcion="Las clásicas canciones del grupo infantil Mazapán en lengua de señas chilena para que los más
                                         pequeños y pequeñas aprendan de manera entretenida a comunicarse a partir de los gestos."
                             boton="Ver"
                             ruta="/libreria/mazapan"/>
             
             <CardHorizontal titulo="Canciones de ayer y hoy"
+                            img="https://static.theclinic.cl/media/2019/02/Captura-de-pantalla-2019-02-20-a-las-18.19.09.png"
                             descripcion="Aprende el poder de la interpretación a través de estas canciones y toda su emoción."
                             boton="Ver"
                             ruta="libreria/canciones" />
             
             <CardHorizontal titulo="Cápsulas educativas"
+                            img="https://www.wazu.cl/site/wp-content/uploads/2022/04/interprete-ls.jpg"
                             descripcion="Si tu intención es ampliar tu vocabulario en Lengua de Señas Chilena estas cápsulas educativas creadas por la Ilustre Municipalidad de San Miguel son para ti."
                             boton="Ver"
                             ruta="libreria/capsulas"/>
             
             <CardHorizontal titulo="Aplicación móvil"
+                            img="https://i.prcdn.co/img?regionKey=sSeACsuM8bLsljMew8hG%2FA%3D%3D"
                             descripcion="Descarga esta app en tu dispositivo móvil, practica en cualquier lugar que estes y sigue aprendiendo."
                             boton="Ver"
                             ruta="libreria/aplicacion"/>
