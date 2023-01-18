64cead7342220ef9da965b4649ff7263aa1d5649
Naturalis007 <tamara.salasvergara@gmail.com>
2023-01-18 15:51:31 +0000

    creacion de view nosotros, enlazado al layout

--- src/js/layout.js
+++ src/js/layout.js
@@ -5,6 +5,8 @@ import ScrollToTop from "./component/scrollToTop";
 import { Home } from "./views/home";
 import { Demo } from "./views/demo";
 import { Single } from "./views/single";
+import { Nosotros } from "./views/nosotros";
+
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -25,6 +27,8 @@ const Layout = () => {
 						<Route path="/" element={<Home />} />
 						<Route path="/demo" element={<Demo />} />
 						<Route path="/single/:theid" element={<Single />} />
+						<Route path="/nosotros" element={<Nosotros />} />
+
 						<Route path="*" element={<h1>Not found!</h1>} />
 					</Routes>
 					<Footer />
---
+++ src/js/views/nosotros.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const Nosotros = () => {
+    return (
+        <div className="container">
+            <div className="row">
+                <h1>Quiénes somos</h1>
+                <p> Somos un grupo de estudiantes de 4Geeks Academy Chile que estamos trabajando en nuestro proyecto final para convertirnos en Desarrolles Full Stack.
+                    Nuestro principal objetivo es ser un aporte real a nuestra sociedad mediante la promoción y apoyo del aprendizaje de la Lengua de Señas Chilena (LSCh), 
+                    siendo este sitio web un lugar en donde se puedan encontrar diferentes recursos audiovisuales para todas aquellas personas que se interesen en 
+                    aprender este tipo de comunicación. 
+                    Nuestra página web es un recurso educativo sin fines de lucro para aprender la lengua de señas chilena. Ofrecemos una biblioteca en donde se pueden encontrar
+                    tutoriales en video, ejercicios interactivos, canciones y aplicaciones móviles que permiten el acceso a este interesante mundo de la lengua de señas. 
+                    Creemos que el aprendizaje de la LSCh es esencial para garantizar la igualdad de oportunidades para las personas sordas y con discapacidad auditiva en nuestro país. 
+                    
+                </p>
+            </div>
+        </div>
+    )
+}
