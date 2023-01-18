8e416b249786051ae69cb43b3da0fc6871973675
Naturalis007 <tamara.salasvergara@gmail.com>
2023-01-18 15:54:55 +0000

    título y descripcion de quines somos

--- src/js/views/nosotros.js
+++ src/js/views/nosotros.js
@@ -5,16 +5,21 @@ export const Nosotros = () => {
         <div className="container">
             <div className="row">
                 <h1>Quiénes somos</h1>
-                <p> Somos un grupo de estudiantes de 4Geeks Academy Chile que estamos trabajando en nuestro proyecto final para convertirnos en Desarrolles Full Stack.
-                    Nuestro principal objetivo es ser un aporte real a nuestra sociedad mediante la promoción y apoyo del aprendizaje de la Lengua de Señas Chilena (LSCh), 
+                <p> Somos un grupo de estudiantes de 4Geeks Academy Chile que se encuentra trabajando en el proyecto final para convertirnos en Desarrolles Full Stack. <br></br>
+                    Nuestro principal objetivo es ser un aporte real a la sociedad mediante la promoción y apoyo del aprendizaje de la Lengua de Señas Chilena (LSCh), 
                     siendo este sitio web un lugar en donde se puedan encontrar diferentes recursos audiovisuales para todas aquellas personas que se interesen en 
-                    aprender este tipo de comunicación. 
+                    aprender este tipo de comunicación. <br></br>
                     Nuestra página web es un recurso educativo sin fines de lucro para aprender la lengua de señas chilena. Ofrecemos una biblioteca en donde se pueden encontrar
-                    tutoriales en video, ejercicios interactivos, canciones y aplicaciones móviles que permiten el acceso a este interesante mundo de la lengua de señas. 
+                    tutoriales en video, ejercicios interactivos, canciones y aplicaciones móviles que permiten el acceso a este interesante mundo de la lengua de señas. <br></br>
                     Creemos que el aprendizaje de la LSCh es esencial para garantizar la igualdad de oportunidades para las personas sordas y con discapacidad auditiva en nuestro país. 
                     
                 </p>
             </div>
+
+            
+
+
+
         </div>
     )
 }
