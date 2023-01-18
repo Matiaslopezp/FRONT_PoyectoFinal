b7a46314c2b58bc96c629f6263ecd1077eeaf57b
Matiaslopezp <lopezp.matias@gmail.com>
2023-01-18 20:18:02 +0000

    save

--- src/js/views/inicio.js
+++ src/js/views/inicio.js
@@ -5,17 +5,32 @@ import "../../styles/inicio.css"
 
 export const Inicio = () => {
     return (
-        <div>
+        <div className="todo">
             <div className="titulo">
-                <p>t-Enseño: Traductor LSCh</p>
+                <img src="https://res.cloudinary.com/dws1jugpv/image/upload/v1674055906/QWE/logo_tense%C3%B1o-removebg-preview_uizjty.png" />
             </div>
-            <div className="carrusel">
-                <Carousel_inicio/>
+
+            <div className="carta1 ">
+
+                <div className="portada">
+                    <div className="logo_carta">
+                        <img src="https://res.cloudinary.com/dws1jugpv/image/upload/c_thumb,w_200,g_face/v1673904209/QWE/WhatsApp_Image_2023-01-14_at_11.05.18_AM_sty0d8.jpg" />
+                    </div>
+                </div>
+
+                <div className="texto">
+                    <div className="parrafo">
+                        <p><strong>Traductor LSM:</strong></p>
+                        <p>Una aplicación para que nosotros nos adaptemos a ELLOS!</p>
+                    </div>
+                </div>
+
             </div>
+
             <div>
-            <div className="logo">
-                <img src="https://res.cloudinary.com/dws1jugpv/image/upload/c_thumb,w_200,g_face/v1673904209/QWE/WhatsApp_Image_2023-01-14_at_11.05.18_AM_sty0d8.jpg" />
-            </div>
+                <div className="logo">
+                    <img src="https://res.cloudinary.com/dws1jugpv/image/upload/c_thumb,w_200,g_face/v1673904209/QWE/WhatsApp_Image_2023-01-14_at_11.05.18_AM_sty0d8.jpg" />
+                </div>
             </div>
         </div>
     )
