41a8ed8d12145abc6ff70024f6912eaa77e691cc
Naturalis007 <tamara.salasvergara@gmail.com>
2023-01-19 15:53:05 +0000

    agregué videos en view cápsulas educativas

--- src/js/views/capsulas.js
+++ src/js/views/capsulas.js
@@ -1,7 +1,48 @@
 import React from "react";
 
 export const Capsulas = ()=> {
-    return <h1>
-        Aqui van las capsulas educativas 
-    </h1>
+    return (
+        <div className="container">
+            <div className="row">
+                <h1> Cápsulas educativas</h1>
+                <h4>Durante el año 2020, en el período de cuarentena debido a la pandemia del covid-19, muchas instituciones inciaron talleres gratuitos y en línea 
+                    para todas las personas que en ese momento quisieran cultivar sus conocimientos en diversas áreas; es así que la Ilustre Municipalidad de San Miguel, de la 
+                    ciudad de Santiago, realizó 6 cápsulas educativas con la finalidad de ampliar y difundir la Lengua de Señas Chilena. Aprenderás a saludar, a deletrear tu 
+                    nombre, las ciudades, los números, los verbos, frutas, verduras, colores, y un largo etc!
+                </h4>
+                <h5>Te invitamos a aprender junto al profesor Jonathan Rojas, ampliando tu vocabulario y manejo de señas utilizadas por convención en nuestro país.
+                </h5>
+            </div>
+
+            <div className="row">
+                <div className="col-6 d-flex p-5"> 
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/xR9XkxUgcI8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div>
+
+                <div className="col-6 d-flex p-5">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/c82O5-KHMKw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div>
+
+                <div className="col-6 d-flex p-5">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/pV4w0jmYZzY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div>
+
+                <div className="col-6 d-flex p-5">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/2PwlO1-Rwek" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div>
+
+                <div className="col-6 d-flex p-5">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/yCdkeuHWDew" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div>
+
+                <div className="col-6 d-flex p-5">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/bGNsUflkuSs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div>
+                
+            
+            </div>
+
+           
+        </div>
+    )
 }
