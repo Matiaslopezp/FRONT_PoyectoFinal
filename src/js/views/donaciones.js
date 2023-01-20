5a7f9e1f00fc8920ec4d3b4e42478b6de99dd03e
catalinasehrt <catalina.dali94@gmail.com>
2023-01-20 15:49:00 +0000

    tabla de banco

--- src/js/views/donaciones.js
+++ src/js/views/donaciones.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../../styles/donaciones.css"
+import {logo} from "../../img/t-enseño-logo.png"
 
 
 
@@ -10,21 +11,31 @@ export const Donaciones = () => (
 	
 
 	
-  <h1 class="display-4"> T-enseño </h1>
-  <p class="lead">Somos una organización sin fines de lucro, con el único objetivo de brindar el apoyo a la comunidad de sordos de nuestro pais.</p>
+  <h1 class="display-4">  </h1>
+  <img src="t-enseño-logo.png" width={100} />
+  <p>Somos una organización sin fines de lucro, con el único objetivo de brindar el apoyo a la comunidad de sordos de nuestro pais.</p>
   
-  <p> en T-enseño creemos que el apoyo entre los distintos sectores de la sociedad es fundamental para hacer del mundo un lugar mejor, por lo mismo, entregamos este espacio donde puedes encontrar la forma de comunicarte y aprender lengua de señas.
+  <p> Creemos que el apoyo entre los distintos sectores de la sociedad es fundamental para hacer del mundo un lugar mejor, por lo mismo, entregamos este espacio donde puedes encontrar la forma de comunicarte y aprender lengua de señas.
 
 Ya sea donando, difundiendo o participando, cada acción cuenta.</p>
-  <p class="lead">
-    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-  </p>
+ 
 </div>
+<div className="Datos">
+
+<ul className="list-group ">
+  <li className="list-group-item disabled" aria-disabled="true">Datos bancarios</li>
+  <li className="list-group-item">Banco : Banco de Chile</li>
+  <li className="list-group-item">Cta. corriente : 99008837434422</li>
+  <li className="list-group-item">Email: t.enseñoseñas@gmail.com</li>
+  <li className="list-group-item">Rut : 88.937.423.5</li>
+  <li className="list-group-item">Nombre : Te enseño señas .LTA</li>
+</ul>
 </div>
 
 
 
 
+</div>
 
 
 
