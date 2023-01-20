1ef596d5500f85fcce404dc34a8db706feb7c01c
catalinasehrt <catalina.dali94@gmail.com>
2023-01-20 18:03:41 +0000

    banco

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -4,49 +4,49 @@ import {logo } from "../../img/t-enseñoManos.png";
 
 export const Navbar = () => {
 	return (
-		<nav class="navbar navbar-expand-lg bg-success">
-  <div class="container-fluid" >
-    <a class="navbar-brand text-light" href="#"> 
+		<nav className="navbar navbar-expand-lg bg-success">
+  <div className="container-fluid" >
+    <a className="navbar-brand text-light" href="#"> 
         
     </a>
     <img src="t-enseñoManos.png" width={95} />
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse ms-4" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto m-2 mb-lg-0">
-        <li class="nav-item">
-        <button class="btn btn-warning text-light m-1" type="submit">Galeria Multimedia</button>
+    <div className="collapse navbar-collapse ms-4" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto m-2 mb-lg-0">
+        <li className="nav-item">
+        <button className="btn btn-warning text-light m-1" type="submit">Galeria Multimedia</button>
           
         </li>
-        <li class="nav-item">
-        <button class="btn btn-warning text-light m-1" type="submit">Traductor de Señas</button>
+        <li className="nav-item">
+        <button className="btn btn-warning text-light m-1" type="submit">Traductor de Señas</button>
          
         </li>
-        <li class="nav-item">
-        <button class="btn btn-warning text-light m-1" type="submit">Sobre nosotros</button>
+        <li className="nav-item">
+        <button className="btn btn-warning text-light m-1" type="submit">Sobre nosotros</button>
        
         </li>
-        <li class="nav-item">
-        <button class="btn btn-warning text-light m-1" type="submit">Donaciones</button>
+        <li className="nav-item">
+        <button className="btn btn-warning text-light m-1" type="submit">Donaciones</button>
           
         </li>
         
                 
         
       </ul>
-      <form class="d-flex" role="registrarse">
+      <form className="d-flex" role="registrarse">
         
-        <button class="btn btn-outline-info" type="submit">Registrarse</button>
+        <button className="btn btn-outline-info" type="submit">Registrarse</button>
       </form>
-      <div class="dropdown">
-  <button class="btn btn-outline-danger dropdown-toggle m-2" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+      <div className="dropdown">
+  <button className="btn btn-outline-danger dropdown-toggle m-2" type="button" data-bs-toggle="dropdown" aria-expanded="false">
      ❤
   </button>
-  <ul class="dropdown-menu dropdown-menu-dark">
-    <li><a class="dropdown-item active" href="#"></a></li>
-    <li><a class="dropdown-item" href="#"></a></li>
-    <li><a class="dropdown-item" href="#"></a></li>
+  <ul className="dropdown-menu dropdown-menu-dark">
+    <li><a className="dropdown-item active" href="#"></a></li>
+    <li><a className="dropdown-item" href="#"></a></li>
+    <li><a className="dropdown-item" href="#"></a></li>
     
   </ul>
 </div>
--- src/js/views/donaciones.js
+++ src/js/views/donaciones.js
@@ -1,54 +1,50 @@
 import React from "react";
-import "../../styles/donaciones.css"
-import {logo} from "../../img/t-enseño-logo.png"
-
-
+import "../../styles/donaciones.css";
+import { logo } from "../../img/t-enseño-logo.png";
 
 export const Donaciones = () => (
-	
-<div className="Jumbotron">
-	<div className="container text-center mt-5 ">
-	
-
-	
-  <h1 class="display-4">  </h1>
-  <img src="t-enseño-logo.png" width={100} />
-  <p>Somos una organización sin fines de lucro, con el único objetivo de brindar el apoyo a la comunidad de sordos de nuestro pais.</p>
-  
-  <p> Creemos que el apoyo entre los distintos sectores de la sociedad es fundamental para hacer del mundo un lugar mejor, por lo mismo, entregamos este espacio donde puedes encontrar la forma de comunicarte y aprender lengua de señas.
-
-Ya sea donando, difundiendo o participando, cada acción cuenta.</p>
- 
-</div>
-<div className="Datos col-4">
-
-<ul className=" container list-group ms-5 ">
-  <li className="list-group-item disabled" aria-disabled="true">Datos bancarios</li>
-  <li className="list-group-item">Banco : Banco de Chile</li>
-  <li className="list-group-item">Cta. corriente : 99008837434422</li>
-  <li className="list-group-item">Email: t.enseñoseñas@gmail.com</li>
-  <li className="list-group-item">Rut : 88.937.423.5</li>
-  <li className="list-group-item">Nombre : Te enseño señas .LTA</li>
-</ul>
-</div>
-
-<div className= " container ">
-<input type="image" src="https://www.paypalobjects.com/es_XC/MX/i/btn/btn_donateCC_LG.gif" name="submit" alt="PayPal, la forma más segura y rápida de pagar en línea." border="0"></input>
-</div>
-
-
-
-</div>
-
-
-
-
-
-
-
-
-
-
-
-	
+  <>
+    <div className="Jumbotron">
+      <div className="container text-center mt-5">
+        <h1 className="display-4"> </h1>
+        <img src="t-enseño-logo.png" width={100} />
+        <p>
+          Somos una organización sin fines de lucro, con el único objetivo de
+          brindar el apoyo a la comunidad de sordos de nuestro pais.
+        </p>
+        <p>
+          Creemos que el apoyo entre los distintos sectores de la sociedad es
+          fundamental para hacer del mundo un lugar mejor, por lo mismo,
+          entregamos este espacio donde puedes encontrar la forma de comunicarte
+          y aprender lengua de señas. Ya sea donando, difundiendo o
+          participando, cada acción cuenta.
+        </p>
+      </div>
+    </div>
+    <div className="Datos col-4 row-1 d-flex">
+      <ul className="container list-group ms-5 pt-5">
+        <li className="list-group-item disabled" aria-disabled="true">
+          Datos Bancarios
+        </li>
+        <li className="list-group-item">Banco : Banco de Chile</li>
+        <li className="list-group-item">Cta. corriente : 99008837434422</li>
+        <li className="list-group-item">Email: t.enseñoseñas@gmail.com</li>
+        <li className="list-group-item">Rut : 88.937.423.5</li>
+        <li className="list-group-item">Nombre : Te enseño señas .LTA</li>
+      </ul>
+    </div>
+    
+    <div className="Pago d-flex">
+      <div className=" container col-4" >
+      <p>Todas las donaciones están protegidas por PAYPAL.</p>
+      <input
+        type="image"
+        src="https://www.paypalobjects.com/es_XC/MX/i/btn/btn_donateCC_LG.gif"
+        name="submit"
+        alt="PayPal, la forma más segura y rápida de pagar en línea."
+        border="0"
+      ></input>
+   </div>
+    </div>
+  </>
 );
