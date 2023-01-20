042f0f37131a73a9583042d99ad867293d28002d
catalinasehrt <catalina.dali94@gmail.com>
2023-01-20 01:32:01 +0000

    donaciones

--- src/js/views/donaciones.js
+++ src/js/views/donaciones.js
@@ -1,17 +1,40 @@
 import React from "react";
-
+import "../../styles/donaciones.css"
 
 
 
 export const Donaciones = () => (
-	<div className="Jumbotron">
 	
-	<div className="text-center mt-5">
-	<div class="p-5 text-center bg-light">
-    <h1 class="mb-3">T-enseño</h1>
-    <h4 class="mb-3">Hacer tu donacion</h4>
-    <a class="btn btn-primary" href="" role="button">presiona aquí</a>
-  </div>
-	</div>
-	 </div>
+<div className="Jumbotron">
+	<div className="container text-center mt-5 ">
+	
+
+	
+  <h1 class="display-4"> T-enseño </h1>
+  <p class="lead">Somos una organización sin fines de lucro, con el único objetivo de brindar el apoyo a la comunidad de sordos de nuestro pais.</p>
+  
+  <p> en T-enseño creemos que el apoyo entre los distintos sectores de la sociedad es fundamental para hacer del mundo un lugar mejor, por lo mismo, entregamos este espacio donde puedes encontrar la forma de comunicarte y aprender lengua de señas.
+
+Ya sea donando, difundiendo o participando, cada acción cuenta.</p>
+  <p class="lead">
+    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+  </p>
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
 );
