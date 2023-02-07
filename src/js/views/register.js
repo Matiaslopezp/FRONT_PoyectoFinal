235757add6db94b342d94c4523cd19eb6b1f18f9
NMoz88 <nesmartinez2@gmail.com>
2023-02-07 02:33:04 +0000

    se modifican register

--- src/js/views/register.js
+++ src/js/views/register.js
@@ -9,18 +9,19 @@ import "../../styles/register.css";
 export const Register = () => {
 	let navigate = useNavigate();
 	return (
-		<div className="container">
-			<div className="todo">
-				<div className="header-1">
-					<img src="https://res.cloudinary.com/dws1jugpv/image/upload/v1674146531/QWE/T_ENSE%C3%91O-removebg-preview_1_qgutke.png" />
-				</div>
-			
+		<div className="todo">
+			<div className="header-1">
+				<img src="https://res.cloudinary.com/dws1jugpv/image/upload/v1674146531/QWE/T_ENSE%C3%91O-removebg-preview_1_qgutke.png" />
 			</div>
+			
+			<div className="header-2">
+                <img src="https://res.cloudinary.com/dws1jugpv/image/upload/v1674493644/QWE/traductor-removebg-preview_ks0wgh.png" />
+            </div>
 
-			<div className="container-sm row justify-content-between ">
+			<div className="row justify-content-evenly">
 
-				<div className="ml-auto container-sm">
-					<div className="panel-header col-4">
+				<div className="col-4">
+					<div className="text-center">
 						<h2 className="panel-heder">
 						HEY! OYEEEE!
 						</h2>
@@ -28,25 +29,30 @@ export const Register = () => {
 						Si aun no te registras presiona y completa el formulario
 						</p>
 					</div>
-					<Link to="/registerform">
-						<button className="btn btn-primary">Register</button>
-					</Link>
+					<div className="text-center">
+						<Link to="/registerform">
+							<button className="btn btn-primary">Register</button>
+						</Link>
+					</div>
 				</div>
 
 
-				<div className="col-4 position-absolute end-0">	
-					<div className="panel-header col-4">
-						<h2 className="panel-heder">
-							HEY! OYEEEE!
-						</h2>
-						<p>
-							Si aun no te registras presiona y completa el formulario
-						</p>
-					</div>
-					<Link to="/registerform">
-						<button className="btn btn-primary">Register</button>
-					</Link>
-					
+				<div className="col-4">	
+					<span className="border border-info">
+						<div className="text-center">
+							<h2 className="panel-heder">
+								HEY! OYEEEE!
+							</h2>
+							<p>
+								Si aun no te registras presiona y completa el formulario
+							</p>
+						</div>
+						<div className="text-center">
+							<Link to="/registerform">
+								<button className="btn btn-primary">Register</button>
+							</Link>
+						</div>
+					</span>
 				</div>
 						
 	
